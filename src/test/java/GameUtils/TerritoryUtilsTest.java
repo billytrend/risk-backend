ce774e1523c77bbbc8d3db6d5f3571555d4316ba
billytrend <billytrend@gmail.com>
2015-02-03 16:18:16 +0000

    Added a test for it.

--- src/test/java/GameUtils/TerritoryUtilsTest.java
+++ src/test/java/GameUtils/TerritoryUtilsTest.java
@@ -1,12 +1,74 @@
 package GameUtils;
 
-import org.junit.Test;
+import GameBuilders.DemoGameBuilder;
+import GameState.State;
+import GameState.Territory;
+import junit.framework.TestCase;
+import org.javatuples.Pair;
 
-public class TerritoryUtilsTest {
+import java.util.HashSet;
 
-	@Test
-	public void test() {
+public class TerritoryUtilsTest extends TestCase {
 
-	}
+    public void testGetAllTerritories() throws Exception {
 
+    }
+
+    public void testGetAllBorders() throws Exception {
+        State st = DemoGameBuilder.buildGame(4, 4);
+        HashSet<Pair<Territory, Territory>> terrs = TerritoryUtils.getAllBorders(st);
+        assertEquals(terrs.size(), 5);
+    }
+
+    public void testAreNeighbours() throws Exception {
+
+    }
+
+    public void testGetUnownedTerritories() throws Exception {
+
+    }
+
+    public void testGetPlayersTerritories() throws Exception {
+
+    }
+
+    public void testHasEmptyTerritories() throws Exception {
+
+    }
+
+    public void testGetNeighbours() throws Exception {
+
+    }
+
+    public void testGetEnemyNeighbours() throws Exception {
+
+    }
+
+    public void testGetFriendlyNeighbours() throws Exception {
+
+    }
+
+    public void testGetTerritoriesWithMoreThanOneArmy() throws Exception {
+
+    }
+
+    public void testGetPossibleAttackingTerritories() throws Exception {
+
+    }
+
+    public void testGetDeployable() throws Exception {
+
+    }
+
+    public void testAddTerritory() throws Exception {
+
+    }
+
+    public void testAddBorder() throws Exception {
+
+    }
+
+    public void testCountTerritories() throws Exception {
+
+    }
 }
