6fc45c1a9534067671b1fb45bca7a8b984e92128
billytrend <billytrend@gmail.com>
2015-02-10 22:28:48 +0000

    Added empty tests.

---
+++ src/test/java/GameUtils/CardUtilsTest.java
@@ -0,0 +1,52 @@
+package GameUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAddCard() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayersCards() throws Exception {
+
+    }
+
+    @Test
+    public void testGivePlayerCard() throws Exception {
+
+    }
+
+    @Test
+    public void testReleaseCard() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUnownedCards() throws Exception {
+
+    }
+
+    @Test
+    public void testGivePlayerRandomCard() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCardsOfType() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPossibleCardCombinations() throws Exception {
+
+    }
+}
