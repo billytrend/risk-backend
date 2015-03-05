00addb0d020e6489ff897ad1060a0a2c7ff3e81f
Paula <pjc8@st-andrews.ac.uk>
2015-03-05 22:43:49 +0000

    added tests, all pass

--- src/test/java/GameEngine/GameSubsetTest.java
+++ src/test/java/GameEngine/GameSubsetTest.java
@@ -27,7 +27,7 @@ public class GameSubsetTest {
 	
 		
 	@Test
-	public void smallMapTest(){
+	public void smallMapTerritoriesConditionTest(){
 		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface()};
 		State gameState = DemoGameBuilder.buildTestGame(2, 15, 4, interfaces);
 		ArrayList<Player> players = gameState.getPlayers();
@@ -44,6 +44,68 @@ public class GameSubsetTest {
 			fail();
 	}
 	
+	
+	@Test
+	public void playersRemainingTest(){
+		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface(),
+				new DumbBotInterface(), new DumbBotInterface(), new DumbBotInterface()};
+		State gameState = DemoGameBuilder.buildTestGame(5, 20, 30, interfaces);
+		ArrayList<Player> players = gameState.getPlayers();
+		WinConditions conditions = new WinConditions(4, 0);
+		GameEngine gameEngine = new GameEngine(gameState, conditions);
+		gameEngine.run();
+		
+		assertEquals(PlayerUtils.countPlayers(gameState), 4);
+	}
+	
+	
+	@Test
+	public void twoConditionsTestOne(){
+		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface(),
+				new DumbBotInterface(), new DumbBotInterface(), new DumbBotInterface()};
+		State gameState = DemoGameBuilder.buildTestGame(5, 20, 30, interfaces);
+		ArrayList<Player> players = gameState.getPlayers();
+		WinConditions conditions = new WinConditions(4, 7);
+		GameEngine gameEngine = new GameEngine(gameState, conditions);
+		gameEngine.run();
+		
+		assertEquals(PlayerUtils.countPlayers(gameState), 5);
+		
+		Player winner = null;
+		for(Player player : players){
+			if(TerritoryUtils.getPlayersTerritories(player).size() == 7){
+				winner = player;
+			}
+		}
+		assertFalse(winner == null);
+		
+		for(Player player : players){
+			if(!winner.equals(player))
+				assertTrue(TerritoryUtils.getPlayersTerritories(player).size() < 7);
+		}
+	}
+	
+	@Test
+	public void twoConditionsTestTwo(){
+		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface(),
+				new DumbBotInterface(), new DumbBotInterface(), new DumbBotInterface()};
+		State gameState = DemoGameBuilder.buildTestGame(5, 20, 30, interfaces);
+		ArrayList<Player> players = gameState.getPlayers();
+		WinConditions conditions = new WinConditions(4, 20);
+		GameEngine gameEngine = new GameEngine(gameState, conditions);
+		gameEngine.run();
+		
+		assertEquals(PlayerUtils.countPlayers(gameState), 4);
+
+		for(Player player : players){
+			assertTrue(TerritoryUtils.getPlayersTerritories(player).size() < 20);
+		}
+	}
+	
+	
+	
+	
+	
 	@Test
 	public void noConditionTest(){
 		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface()};
@@ -58,25 +120,27 @@ public class GameSubsetTest {
 	}
 	
 	@Test
-	public void mediumMapTest(){
+	public void mediumMapTerritoriesConditionTest(){
 		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface()};
 		State gameState = DemoGameBuilder.buildTestGame(2, 30, 16, interfaces);
 		ArrayList<Player> players = gameState.getPlayers();
 		WinConditions conditions = new WinConditions(1, 14);
 		GameEngine gameEngine = new GameEngine(gameState, conditions);
+		
 		gameEngine.run();
 		
 		assertEquals(PlayerUtils.countPlayers(gameState), 2);
 		if(TerritoryUtils.getPlayersTerritories(players.get(0)).size() == 14)
 			assertEquals(TerritoryUtils.getPlayersTerritories(players.get(1)).size(), 2);
-		else if((TerritoryUtils.getPlayersTerritories(players.get(1)).size() == 2))
-			assertEquals(TerritoryUtils.getPlayersTerritories(players.get(0)).size(), 14);
-		else
+		else if(TerritoryUtils.getPlayersTerritories(players.get(1)).size() == 14)
+			assertEquals(TerritoryUtils.getPlayersTerritories(players.get(0)).size(), 2);
+		else{
 			fail();
+		}
 	}
 	
 	@Test
-	public void morePlayersTest(){
+	public void morePlayersTerritoriesConditionTest(){
 		PlayerInterface[] interfaces = new PlayerInterface[]{new DumbBotInterface(), new DumbBotInterface(),
 				new DumbBotInterface(), new DumbBotInterface(), new DumbBotInterface()};
 		State gameState = DemoGameBuilder.buildTestGame(5, 100, 70, interfaces);
